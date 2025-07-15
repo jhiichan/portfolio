43bb87bf2cb6b8345c5ee53b3e1e74e3fbf57c42
Lloyd John B <lloyd.john.b@outlook.com>
2025-07-15 20:08:35 +0800

    Create Experience History Component

---
+++ src/components/organisms/Experience/Experience.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import moment from 'moment';
+import { Container, ExperiencesContainer } from './elements';
+
+type ExperienceHistory = {
+  companyName: string;
+  positions: {
+    title: string;
+    startDate: Date;
+    endDate?: Date;
+    location: string;
+    description: string;
+  }[];
+};
+
+type Props = {
+  experiences?: ExperienceHistory[];
+};
+
+const Component = ({ experiences }: Props) => {
+  return (
+    <Container>
+      <ExperiencesContainer>
+        {experiences?.map((exp, expIdx) => (
+          <div key={expIdx} className='experience-row-container'>
+            <div className='company-container'>
+              <Typography variant='h5'>{exp.companyName}</Typography>
+            </div>
+            {exp.positions.map((pos, posIdx) => {
+              const endDate = pos.endDate ?? new Date();
+              const totalMonths = moment(endDate).diff(pos.startDate, 'months');
+              const years = Math.floor(totalMonths / 12);
+              const months = totalMonths % 12;
+              const yearsText = years ? `${years} yrs` : '';
+              const monthsText = months ? `${months} mos` : '';
+
+              return (
+                <div key={`${expIdx}-${posIdx}`} className='position-container'>
+                  <Typography variant='h6'>{pos.title}</Typography>
+                  <Typography variant='caption'>{`${moment(pos.startDate).format('MMM YYYY')} - ${pos.endDate ? moment(pos.endDate).format('MMM YYYY') : 'Present'} • ${yearsText} ${monthsText}`}</Typography>
+                  <br />
+                  <Typography variant='caption'>{pos.location}</Typography>
+                  <Typography variant='body1' className='description'>
+                    {pos.description}
+                  </Typography>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </ExperiencesContainer>
+    </Container>
+  );
+};
+
+export default Component;
+export type { Props };
