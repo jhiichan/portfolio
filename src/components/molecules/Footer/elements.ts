9205b5b5414b09ca82c28203198557747a2b8719
Lloyd John B <lloyd.john.b@outlook.com>
2025-07-19 10:13:04 +0800

    Modify Footer component

--- src/components/molecules/Footer/elements.ts
+++ src/components/molecules/Footer/elements.ts
@@ -1,6 +1,6 @@
-import styled from '@emotion/styled';
-import { Box, type Theme } from '@mui/material';
-import { DEVICE_MEDIA_QUERIES } from '../../../constants';
+import styled from "@emotion/styled";
+import { Box, type Theme } from "@mui/material";
+import { DEVICE_MEDIA_QUERIES } from "../../../constants";
 
 const Container = styled(Box)<{ theme?: Theme }>`
   display: flex;
@@ -8,13 +8,25 @@ const Container = styled(Box)<{ theme?: Theme }>`
   padding: 16px 0;
   background-color: ${({ theme }) => theme.palette.grey[900]};
 
+  .footer-content {
+    flex-direction: row;
+  }
+
   .copyright-container {
     padding: 8px;
     color: ${({ theme }) => theme.palette.text.primary};
   }
 
-  @media ${DEVICE_MEDIA_QUERIES.phones}, ${DEVICE_MEDIA_QUERIES.tablets} {
+  @media ${DEVICE_MEDIA_QUERIES.tablets} {
     padding: 16px 20px;
+  }
+
+  @media ${DEVICE_MEDIA_QUERIES.phones} {
+    padding: 16px 20px;
+
+    .footer-content {
+      flex-direction: column-reverse;
+    }
   }
 `;
 
